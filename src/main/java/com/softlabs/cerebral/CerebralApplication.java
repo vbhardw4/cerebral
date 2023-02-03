91e5fbcf17a4f042cfe07c7924cb40b556c98849
vbhardw4 <vbhardw4@uwo.ca>
2023-02-03 16:58:37 -0500

    Initial commit of Spring Boot Application

---
+++ src/main/java/com/softlabs/cerebral/CerebralApplication.java
@@ -0,0 +1,13 @@
+package com.softlabs.cerebral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softlabs/cerebral/CerebralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softlabs.cerebral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerebralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
